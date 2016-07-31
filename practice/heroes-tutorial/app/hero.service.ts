be895b485a1b9b21eb5327b57916dcdb3bddd42f
Robin Smith <robin@rsmith.io>
2016-07-31 15:38:57 +0100

    Working with in-memory http service

--- practice/heroes-tutorial/app/hero.service.ts
+++ practice/heroes-tutorial/app/hero.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
-//import * as _ from 'lodash';
+import 'rxjs/add/operator/toPromise';
 
-import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+    private heroesUrl = 'app/heroes';  // URL to web api
+
+    constructor(private http: Http) { }
+
     getHeroes() {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     }
 
     getHero(id) {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    private handleError(error: any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     }
 }
